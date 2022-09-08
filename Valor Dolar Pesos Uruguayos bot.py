ab411000231426ebfdf10c3a24731e24f635bb3a
Bruno Silveira <brunosil_94@hotmail.com>
2022-09-08 12:39:22 -0300

    Update Valor Dolar Pesos Uruguayos bot.py

--- Valor Dolar Pesos Uruguayos bot.py
+++ Valor Dolar Pesos Uruguayos bot.py
@@ -1,7 +1,5 @@
-# Instalar previamente las siguientes librerias:
 # pip install selenium
 # pip install webdriver-manager
-# pip install bs4
 
 #libraries
 from time import sleep
@@ -11,147 +9,116 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 
-#------------------------------- START OF WEB SCRAPPING -----------------------------
+# Init
+currency = "Dolar USA" # Opciones: Real, Euro, Peso Argentino
+email = "mail@gmail.com"
+contrasenia = "contraseña"
+telefono = "091111111"
 
-url = 'https://www.ine.gub.uy/cotizacion-de-monedas' # El web scrapping del valor de las monedas se consigue desde la pagina del Insituto Nacional de Estadistica, es la mas estable y con valores de otras monedas
-html = urlopen(url).read()
-soup = BeautifulSoup(html, features="html.parser")
-
-# Limpiamos y formateamos la salida:
-# kill all script and style elements
-for script in soup(["script", "style"]):
-    script.extract()    # rip it out
-# get text
-text = soup.get_text()
-# break into lines and remove leading and trailing space on each
-lines = (line.strip() for line in text.splitlines())
-# break multi-headlines into a line each
-chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-# drop blank lines
-text = '\n'.join(chunk for chunk in chunks if chunk)
-
-day = datetime.today().day # PARA CONSEGUIR LA COTIZACION DEL DIA DE ACTUAL
-#day = 21 # PARA FORZAR LA COTIZACION DE UN DIA ESPECIFICO DENTRO DEL MES, PONER EL DIA
-index = int(text.find("Dolar USA"+str(day).zfill(2)))
-
-# si la cotizacion del dia no esta disponible todavia
-if index == -1:
-    print("Cotizacion no disponible. Finalizado.")
-    exit()
-
-# obtengo los chars y los convierto a un string, luego separo venta y compra
-values = []
-for i in range(index+9,index+37):
-        values.append(text[i]) 
-value = "".join(values)  #Ejemplo: Dolar USA30/08/2022 09:0039.450041.8500
-
-#estos values[x:y] son substrings para, por posicion, leer solo los valores de compra y venta  
-compra = value[16:21]
-venta = value[23:29]
-
-print("Valor compra actual: "+compra)
-
-#valor compra del dia anterior valido
-fecha_actual = value[0:10]
-fecha_actual = datetime.strptime(fecha_actual,"%d/%m/%Y").date()
-
-number_of_week = fecha_actual.weekday()
-
-# si es lunes mirar la fecha del ultimo viernes
-if number_of_week == 0:
-    dia_anterior = fecha_actual - timedelta(days=3)
-else:
-    dia_anterior = fecha_actual - timedelta(days=1)
-    
-new_dia_anterior = dia_anterior.strftime('%d/%m/%Y')
-index = int(text.find("Dolar USA"+str(new_dia_anterior).zfill(2)))
-
-values = []
-for i in range(index+9,index+37):
-        values.append(text[i]) 
-value_anterior = "".join(values) 
- 
-compra_anterior = value_anterior[16:21]
-venta_anterior = value_anterior[23:29]
-
-print("Valor compra anterior: "+compra_anterior)
-
-#calculamos la diferencia con el ultimo dia valido y vemos si subio, bajo o se mantuvo
-compra = float(compra)
-compra_anterior = float(compra_anterior)
-diff = compra - compra_anterior
-direccion = "Subiendo"
-percentage = 100-((compra_anterior*100)/compra)
-percentage = str(round(percentage,2)) + " %"
-if diff < 0:
-    diff = compra_anterior-compra
-    direccion  = "Bajando"
-    percentage = 100-((compra*100)/compra_anterior)
-    percentage = str(round(percentage,2)) + " %"
-if diff == 0:
-    direccion = "Manteniendose igual"
-    percentage = ""
-
-#------------------------------- END OF WEB SCRAPPING -----------------------------
-#------------------------------- START OF TWEET FORMATTING ------------------------
-
-tweet = """El valor del dólar a la fecha es de: 
-"""+str(compra)+""" pesos uruguayos
-"""+direccion+""" """+percentage+"""
-Fuente: https://ine.gub.uy     
-#bot #dolar #pesos #uruguay #cotizacion #BCU #brou""" 
-
-#------------------------------- END OF TWEET FORMATTING --------------------------
-#------------------------------------ START SELENIUM ------------------------------
-
-#configuration
+# Configuracion Selenium
 options = Options()
-#options.add_argument("start-maximized")
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
 #driver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
 
-#open webpage
-driver.get("https://twitter.com/i/flow/login")
-print("Pagina abierta")
+# INE
+print("Obteniendo datos del INE...")
+driver.get("https://www.ine.gub.uy/cotizacion-de-monedas")
 
-sleep(15)
+cells = driver.find_elements(By.XPATH,"//td[@role='gridcell']")
+
+dolar_data = []
+index = 0
+loop = False
+for data in cells:
+    if data.text == currency:
+        index = 0
+        loop = True    
+    if loop and index < 4:
+        if index == 1:
+            fecha = data.text[0:10]
+            dolar_data.append(fecha)
+        else:
+            dolar_data.append(data.text)
+        index=index+1
+    else:
+        loop = False
+
+fecha_actual = datetime.today()
+#fecha_actual = fecha_actual - timedelta(days=4) # DESCOMENTAR Y AJUSTAR EL days=x PARA FORZAR LA COTIZACION DE x DIAS ANTERIORES AL ACTUAL
+fecha_actual = str(fecha_actual.day).zfill(2)+"/"+str(fecha_actual.month).zfill(2)+"/"+str(fecha_actual.year)
+
+try:
+    dolar_data.index(fecha_actual)
+except:
+    print("Cotizacion no disponible. Finalizado.")
+    exit()
+
+compra_actual = dolar_data[2]
+compra_anterior = dolar_data[6]
+venta_actual = dolar_data[3] 
+venta_anterior = dolar_data[7]
+print("Valor compra anterior: "+ compra_anterior)
+print("Valor compra actual: "+ compra_actual)
+
+# calculamos la diferencia con el ultimo dia habil y vemos si subio o bajo
+diff = float(compra_actual) - float(compra_anterior)
+if diff < 0:
+    direccion  = "Bajando"
+    percentage = str(round(100-((float(compra_actual)*100)/float(compra_anterior)),2)) + " %"
+elif diff == 0:
+    direccion = "Manteniendose igual"
+    percentage = ""
+else:
+    direccion = "Subiendo"
+    percentage = str(round(100-((float(compra_anterior)*100)/float(compra_actual)),2)) + " %"
+    
+compra_actual = str(round(float(compra_actual),2))
+
+#------------------------------- START OF TWEET FORMATTING ------------------------
+tweet = """El valor del dólar a la fecha es de: 
+"""+compra_actual+""" pesos uruguayos
+"""+direccion+""" """+percentage+"""
+Fuente: https://ine.gub.uy     
+#bot #dolar #pesos #uruguay #cotizacion #BCU #brou""" 
+#------------------------------- END OF TWEET FORMATTING --------------------------
+
+# TWITTER
+driver.get("https://twitter.com/i/flow/login")
+print("Entrando a Twitter...")
+sleep(10)
 
 email = driver.find_element(By.XPATH,"//input[@name='text']")
-email.send_keys("correo@gmail.com")
+email.send_keys(email)
 email.send_keys(Keys.TAB+Keys.ENTER)
 print("Email ingresado")
+sleep(10)
 
-sleep(15)
-
-try: 
-    # estas lineas son porque Twitter de forma aleatoria pregunta el numero de movil para evitar cuentas falsas, si lo pide, el bot lo ingresa, debe de estar previamente configurado en twitter
+try:
     email = driver.find_element(By.XPATH,"//input[@name='text']")
-    email.send_keys("091111111")
+    email.send_keys(telefono)
     email.send_keys(Keys.TAB+Keys.ENTER)
     print("Telefono ingresado")
 except:
     print("Ingreso de telefono no necesario")
 finally:
-    sleep(15)
+    sleep(10)
     password = driver.find_element(By.XPATH,"//input[@name='password']")
-    password.send_keys("CoNtRaSeÑaDeTwItTeR")
+    password.send_keys(contrasenia)
     password.send_keys(Keys.TAB+Keys.TAB+Keys.TAB+Keys.ENTER)
     print("Password ingresada")
-
-sleep(15)
-
+    
+sleep(10)
 tweet_box = driver.find_element(By.XPATH,"//div[@class='public-DraftStyleDefault-block public-DraftStyleDefault-ltr']")
 tweet_box.send_keys(tweet)
 tweet_box.send_keys(Keys.TAB+Keys.TAB+Keys.TAB+Keys.TAB+Keys.TAB+Keys.TAB+Keys.TAB+Keys.TAB+Keys.ENTER)
 print("Tweet publicado")
-
 sleep(5)
 
 driver.close()
 driver.quit()
-#------------------------------------ END OF SELENIUM -----------------------------

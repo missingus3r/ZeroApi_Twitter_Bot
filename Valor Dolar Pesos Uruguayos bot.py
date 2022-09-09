ce10e76e59ed5516660440277302868aca642c05
Bruno Silveira <brunosil_94@hotmail.com>
2022-09-09 20:31:10 -0300

    Update Valor Dolar Pesos Uruguayos bot.py

--- Valor Dolar Pesos Uruguayos bot.py
+++ Valor Dolar Pesos Uruguayos bot.py
@@ -19,9 +19,9 @@ telefono = "091111111"
 
 # Configuracion Selenium
 options = Options()
-# options.add_argument("--headless")
-# options.add_argument("--no-sandbox")
-# options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
 #driver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
